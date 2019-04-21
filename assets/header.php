6a97e54260fcc11f984faa192a37b20def961380
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-04-21 19:13:31 +0000

    update

--- assets/header.php
+++ assets/header.php
@@ -1,17 +1,27 @@
 <div id="top-main-header">
+    <div class="container">
       <div id="social-nav">
-        <img src="images/fb-logo.svg" alt="Página no facebook da instituição">
-        <img src="images/insta-logo.svg" alt="">
+        <img src="images/icons/fb-logo.svg" alt="Página no Facebook da instituição">
+        <img src="images/icons/insta-logo.svg" alt="Página no Instagram da instituição">
       </div>
-      <div id="portal-nav"></div>
+      <div id="portal-nav"><img src="images/icons/icon-portal-expand.svg" alt="Mostrar links dos portais"></div>
+    </div>
 </div>
 
 <header id="main-header">
-   <div id="left-logo"></div>
-   <div id="center-container">
-    <div id="nav-main"></div>
-    <div id="main-search"></div>
-    <div id="sub-nav-main"></div>
-   </div>
-   <div id="right-logo"></div>
+  <div class="container">
+    <div id="left-logo">
+      <img src="images/logos/IABRB-logo.svg" alt="Instituto Anglicano Barão do Rio Branco">
+      <span class="logo-context">Instituto Anglicano Barão do Rio Branco</span>
+    </div>
+    <div id="center-container">
+      <div id="nav-main"></div>
+      <div id="main-search"></div>
+      <div id="sub-nav-main"></div>
+    </div>
+    <div id="right-logo">
+      <img src="images/logos/FAE-logo.svg" alt="Faculdade Anglicana de Erechim">
+      <span class="logo-context">Faculdade Anglicana de Erechim</span>
+    </div>
+  </div>   
 <header>

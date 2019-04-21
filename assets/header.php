f289f6ae3f0635d9aaf38ceb04e4457bf2323b8f
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-04-21 13:47:15 -0300

    start project

---
+++ assets/header.php
@@ -0,0 +1,17 @@
+<div id="top-main-header">
+      <div id="social-nav">
+        <img src="images/fb-logo.svg" alt="Página no facebook da instituição">
+        <img src="images/insta-logo.svg" alt="">
+      </div>
+      <div id="portal-nav"></div>
+</div>
+
+<header id="main-header">
+   <div id="left-logo"></div>
+   <div id="center-container">
+    <div id="nav-main"></div>
+    <div id="main-search"></div>
+    <div id="sub-nav-main"></div>
+   </div>
+   <div id="right-logo"></div>
+<header>
